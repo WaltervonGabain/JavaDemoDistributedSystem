fb7d9767da4c46b609f324070a520a494af2a5a3
Walter von Gabain <waltervongabain@hotmail.com>
2020-05-08 15:07:19 +0200

    Update

--- kubernetes/build_push_deploy.sh
+++ kubernetes/build_push_deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-cd ..
-
-cd inputsender/src/
+cd inputsender
 if podman build -t $(minikube ip):5000/inputsender . ; then
   podman push $(minikube ip):5000/inputsender
 fi
-cd ..
+
 cd ..
 
-cd inputhandler/src/
+cd inputhandler
 if podman build -t $(minikube ip):5000/inputhandler . ; then
   podman push $(minikube ip):5000/inputhandler
 fi
