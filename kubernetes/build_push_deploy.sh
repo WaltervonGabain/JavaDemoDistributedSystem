acf5488818fc6b636fc9b1f8b2aae3be24f03b46
Walter von Gabain <waltervongabain@hotmail.com>
2020-05-11 11:11:36 +0200

    Final changes pod networking

--- kubernetes/build_push_deploy.sh
+++ kubernetes/build_push_deploy.sh
@@ -13,3 +13,10 @@ cd InputSender
 if podman build -t $(minikube ip):5000/inputsender . ; then
   podman push $(minikube ip):5000/inputsender
 fi
+
+cd ../..
+cd kubernetes/
+
+kubectl apply -f inputhandler.yaml
+kubectl expose deployment/inputhandler
+kubectl describe svc inputhandler
